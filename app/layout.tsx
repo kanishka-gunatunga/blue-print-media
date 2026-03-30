7e4259fd673e7bfec2155a2846c782d8c740af98
Kanishka Gunatunga <130125611+kanishka-gunatunga@users.noreply.github.com>
2026-03-30 16:52:28 +0530

    twak to widget added 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Libre_Baskerville } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
