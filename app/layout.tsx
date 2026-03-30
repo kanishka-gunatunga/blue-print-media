d3c74bd42a5a288ef741f4e24404ff0861ceb94d
Kanishka Gunatunga <130125611+kanishka-gunatunga@users.noreply.github.com>
2026-03-30 16:51:21 +0530

    twak to widget added

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,19 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col font-sans" suppressHydrationWarning>
+        <Script id="tawk-to" strategy="afterInteractive">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/69ca5bb045372f1c4ad3ea93/1jkv7cb82';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
         {children}
       </body>
     </html>
